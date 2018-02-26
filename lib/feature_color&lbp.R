58d6279bbb01310994726265251f9950792a948f
Wanting <wc2628@columbia.edu>
2018-02-25 19:02:58 -0500

    Added features
    Added color(RGB&HSV) feature code as well as feature data

--- lib/feature_color&lbp.R
+++ lib/feature_color&lbp.R
@@ -12,21 +12,32 @@ feature <- function(img_dir){
   ### Sample simple feature: Extract row average raw pixel values as features
   
   ### Input: a directory that contains images ready for processing
-  ### Output: an .RData file contains processed features for the images
+  ### Output: an .RData file contains processed features for the images: 
+  ###         1.Color features (RGB and HSV features, 2000*2001 dataframe), 
+  ###         2. Gray features (LBP feature, 2000*31 dataframe)
+  
+  ### Takes about 20min in total
+  
   
   ### load libraries
   library("EBImage")
   library(reticulate)
+  library("wvtool")
   cv2 <- reticulate::import('cv2')
-
+  
   # Set the parameters for visual feature extractions
   n_resize <- 128 # the uniform size we want for all the images
   n_intervals <- 10 # How many levels we want to set for red/green/blue intensity
+  n_intervals_LBP <- 30 # How many levels we want to set for LBP intensity
+  
   n_files <- length(list.files(img_dir)) # number of files in the directory
   n_names <- list.files(img_dir) # the names of all the images in the directory
   intervals_RGB <- seq(0,1,length.out = n_intervals) # the limits for the levels
   intervals_value <- seq(0,0.005, length.out = n_intervals) # the limits for the levels for the V in HSV
+  intervals_LBP <- seq(0,255, length.out = n_intervals_LBP) # the limits for the levels for LBP features
+  pb <- txtProgressBar(min = 0, max = n_files, style = 3) # Make a progress bar
   
+
   # Construct the RGB feature matrix and rename the columns and rows.
   RGB <- data.frame(matrix(NA, n_files, n_intervals^3+1))
   colnames(RGB) <- c("Image", paste("RGB_", 1:n_intervals^3, sep = ""))
@@ -37,9 +48,15 @@ feature <- function(img_dir){
   colnames(HSV) <- c("Image", paste("HSV_", 1:n_intervals^3, sep = ""))
   HSV$Image <- n_names
   
+  # Construct the LBP feature matrix and rename the columns and rows.
+  LBP <- data.frame(matrix(NA, n_files, n_intervals_LBP+1))
+  colnames(LBP) <- c("Image", paste("LBP_", 1:n_intervals_LBP, sep = ""))
+  LBP$Image <- n_names
+  
   
   # Then run a loop to read all the images, resize them, and factor out their color intensity to store in the feature matrix.
   for(i in 1:n_files){
+    
     img <- readImage(paste(img_dir, n_names[i], sep = ""))
     img <- resize(img, n_resize, n_resize)
     img_data <- imageData(img)
@@ -55,7 +72,7 @@ feature <- function(img_dir){
     
     # HSV
     # We have to set the dimension of the img matrix to convert the rgb to hsv format.
-    dim(img_data) <- c(n_resize*n_resize,3) 
+    dim(img_data) <- c(n_resize*n_resize,3)
     HSV_data <- rgb2hsv(t(img_data))
     HSV_levels <- as.data.frame(table(
       factor(findInterval(HSV_data[1,], intervals_RGB), levels = 1:n_intervals),
@@ -63,9 +80,21 @@ feature <- function(img_dir){
       factor(findInterval(HSV_data[3,], intervals_value), levels = 1:n_intervals)
     ))
     HSV[i,2:ncol(HSV)] <- HSV_levels$Freq/(n_resize^2)
+    
+    # LBP
+    img_gray <- cv2$cvtColor(np_array(img, dtype='float32'), cv2$COLOR_BGR2GRAY)
+    lbp_data <- lbp(img_gray)$lbp.ori
+    lbp_levels <- as.data.frame(table(
+      factor(findInterval(lbp_data, intervals_LBP), levels = 1:n_intervals_LBP)
+    ))
+    LBP[i, 2:ncol(LBP)] <- lbp_levels$Freq/(n_resize^2)
+
+    # update progress bar
+    setTxtProgressBar(pb, i)
+    
   }
-  
+  close(pb)
   color_features <- merge(RGB, HSV, by.x = "Image")
-  return(color_features)
+  return(list(color = color_features, gray = LBP))
   
 }
